bf46a7ee66498294a148baa5dff2b9e9d6ccedcc
Thaha Mohammed <thaha.mohammed@aalto.fi>
2022-06-16 23:37:45 +0300

    Fix styling issues and gosec recommendation

--- pkg/fix/fix.go
+++ pkg/fix/fix.go
@@ -16,7 +16,7 @@ type Pdf struct {
 
 // Fix binaryName can be the name/path of the gs binary obtained with `which` in Linux and `where` in Windows
 // The function does not clean up the temp directory handle it separate using the dirPath returned
-// TODO: Implememnt docker based fix
+// TODO: Implement docker based fix
 func (p *Pdf) Fix(gsBinary string) (err error) {
 	tempDir := "/tmp"
 	tempFile := "/temp-pub.pdf"
@@ -39,10 +39,13 @@ func (p *Pdf) Fix(gsBinary string) (err error) {
 	p.TmpFile = p.TmpDir + tempFile
 	txtFile := strings.ReplaceAll(p.TmpFile, ".pdf", ".txt")
 
-	gsCmd := exec.Command(gsBinary, "-q", "-sDEVICE=pdfwrite", "-dNOPAUSE", "-dBATCH", "-dSAFER", "-dFirstPage=1", "-dLastPage=1",
-		"-sOutputFile="+p.TmpFile, p.Name)
+	cmdArgs := "-sOutputFile=" + p.TmpFile
 
-	gsCmdTxt := exec.Command(gsBinary, "-q", "-sDEVICE=txtwrite", "-dNOPAUSE", "-dBATCH", "-dSAFER", "-sOutputFile="+txtFile, p.TmpFile)
+	gsCmd := exec.Command(gsBinary, "-q", "-sDEVICE=pdfwrite", "-dNOPAUSE", "-dBATCH", "-dSAFER", "-dFirstPage=1", "-dLastPage=1",
+		cmdArgs, p.Name)
+
+	cmdArgsTxt := "-sOutputFile=" + txtFile
+	gsCmdTxt := exec.Command(gsBinary, "-q", "-sDEVICE=txtwrite", "-dNOPAUSE", "-dBATCH", "-dSAFER", cmdArgsTxt, p.TmpFile)
 
 	err = gsCmd.Run()
 	if err != nil {
